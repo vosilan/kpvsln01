fbf8f2d911ef30bb342de9b3de7767ab28ccea29
vosilan <vovasila06@yandex.ru>
2025-09-26 13:56:49 +0300

    Init commit

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import socket
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_username():
+    """Получаем имя пользователя разными способами"""
+    try:
+        # Попробуем разные методы
+        return os.getlogin()
+    except (OSError, AttributeError):
+        try:
+            return os.environ.get('USER') or os.environ.get('USERNAME') or 'unknown'
+        except:
+            return 'unknown'
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_hostname():
+    """Получаем имя хоста"""
+    try:
+        return socket.gethostname()
+    except:
+        return 'localhost'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_current_dir():
+    """Получаем текущую директорию с заменой домашней на ~"""
+    try:
+        current_dir = os.getcwd()
+        home_dir = os.path.expanduser("~")
+
+        if current_dir == home_dir:
+            return "~"
+        elif current_dir.startswith(home_dir + os.sep):
+            return "~" + current_dir[len(home_dir):]
+        else:
+            return current_dir
+    except:
+        return "?"
+
+
+def get_console_prompt():
+    """Формируем приглашение консоли"""
+    username = get_username()
+    hostname = get_hostname()
+    current_dir = get_current_dir()
+
+    return f"{username}@{hostname}:{current_dir}$ "
+
+
+# Использование
+if __name__ == "__main__":
+    prompt = get_console_prompt()
+    print(prompt)
