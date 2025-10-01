3a6feae602d557f42338bd45fbff96279b40855e
vosilan <vovasila06@yandex.ru>
2025-10-01 15:07:57 +0300

    Task 2 emu.py

--- emu.py
+++ emu.py
@@ -1,16 +1,45 @@
 import os
 import socket
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vfs')
+    parser.add_argument('--script')
+    args = parser.parse_args()
+
+    print(f"DEBUG: VFS={args.vfs}, SCRIPT={args.script}")
+
+    if args.script:
+        if os.path.exists(args.script):
+            with open(args.script, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    if line and not line.startswith('#'):
+                        print(f"$ {line}")
+                        parts = line.split()
+                        cmd = parts[0]
+                        args_cmd = parts[1:] if len(parts) > 1 else []
+
+                        if cmd == "ls":
+                            print(f"ls: {' '.join(args_cmd)}")
+                        elif cmd == "cd":
+                            print(f"cd: {' '.join(args_cmd)}")
+                        elif cmd == "exit":
+                            break
+                        else:
+                            print(f"{cmd}: команда не найдена")
+        else:
+            print(f"Ошибка: скрипт {args.script} не найден")
+            return
+
     while True:
         user = os.getlogin()
         hostname = socket.gethostname()
-
         print(f"{user}@{hostname}$ ", end="")
         cmd_input = input().strip()
 
-        # Парсер: раскрываем переменные окружения
         parts = []
         for part in cmd_input.split():
             if part.startswith('$'):
